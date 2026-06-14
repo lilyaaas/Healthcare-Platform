22449199c75cb8bb9564750e82d8efcb9ff74f5d
lilyaaas <ilyasslhouari@gmail.com>
2026-06-14 20:34:35 +0100

    add: integrate authentication routes into the main application

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require('./modules/auth/routes');
+
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// The Auth Module
+app.use('/api/auth', authRoutes);
 
 // Check Route
 app.get("/", (req, res) => {
